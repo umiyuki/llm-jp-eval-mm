15d161932cb3d02848976143c8462c5657fd6618
speed1313 <speedtry13@icloud.com>
2024-08-29 00:15:15 +0900

    add example models

---
+++ examples/llava_1_6.py
@@ -0,0 +1,50 @@
+import torch
+
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
+import torch
+
+
+class VLM:
+    def __init__(self) -> None:
+        self.model_id = "llava-hf/llava-v1.6-mistral-7b-hf"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = LlavaNextForConditionalGeneration.from_pretrained(
+            self.model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True
+        )
+        self.processor = LlavaNextProcessor.from_pretrained(self.model_id)
+        self.model.to(self.device)
+
+    def generate(self, image, text: str):
+        conversation = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": text},
+                    {"type": "image"},
+                ],
+            },
+        ]
+
+        prompt = self.processor.apply_chat_template(
+            conversation, add_generation_prompt=True
+        )
+
+        inputs = self.processor(images=image, text=prompt, return_tensors="pt").to(
+            "cuda:0"
+        )
+
+        # autoregressively complete prompt
+        output = self.model.generate(**inputs, max_new_tokens=100)
+
+        generated_text = self.processor.decode(output[0], skip_special_tokens=True)
+        # split [INST] and return the last part
+        generated_text = generated_text.split("[/INST]")[-1].strip()
+        return generated_text
+
+if __name__ == "__main__":
+    import requests
+    from PIL import Image
+    model = VLM()
+    image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(image_file, stream=True).raw)
+    print(model.generate(image, "What is in the image?"))
