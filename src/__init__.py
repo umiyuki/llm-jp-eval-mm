b380eebdfbdf4ad1ae1751576c26d3f05a99c219
speed1313 <speedtry13@icloud.com>
2024-09-01 18:18:57 +0900

    fix .env loading  script

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
+from os.path import join, dirname, abspath
+
+dotenv_path = join(dirname(dirname(abspath(__file__))), ".env")
+print("Loading .env file from", dotenv_path)
+load_dotenv(dotenv_path)
 
 from . import api, tasks
-
-# TODO: Please load your OWN .env file
-load_dotenv("/home/silviase/llmjp/llm-jp-eval-multimodal/.env")
