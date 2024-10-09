81a402d06fa322801803f939e5e26b2cd1128a59
speed1313 <speedtry13@icloud.com>
2024-10-09 11:33:39 +0900

    add jmmmu task

--- src/eval_mm/tasks/__init__.py
+++ src/eval_mm/tasks/__init__.py
@@ -1,3 +1,4 @@
 from .ja_vg_vqa_500 import JaVGVQA500
 from .japanese_heron_bench import JapaneseHeronBench
 from .ja_vlm_bench_in_the_wild import JaVLMBenchIntheWild
+from .jmmmu import JMMMU
--- src/eval_mm/tasks/ja_vlm_bench_in_the_wild.py
+++ src/eval_mm/tasks/ja_vlm_bench_in_the_wild.py
@@ -1,4 +1,4 @@
-from datasets import Dataset, concatenate_datasets, load_dataset
+from datasets import Dataset, load_dataset
 from tqdm import tqdm
 
 from ..api.registry import register_task
