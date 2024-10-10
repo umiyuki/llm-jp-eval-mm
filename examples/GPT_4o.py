3ad266c95629790d191d300cfa5044f8ff9ed056
speed1313 <speedtry13@icloud.com>
2024-10-10 17:05:54 +0900

    add more models

---
+++ examples/GPT_4o.py
@@ -0,0 +1,85 @@
+import torch
+from openai import AzureOpenAI
+import os
+from io import BytesIO
+import base64
+import requests
+
+def encode_image_to_base64(image):
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    return img_str
+
+class VLM:
+    model_id: str = "gpt-4o-2024-05-13"
+    def __init__(self) -> None:
+
+        self.client = AzureOpenAI(
+                api_key=os.getenv("AZURE_OPENAI_KEY"),
+                api_version="2023-05-15",
+                azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            )
+
+    def generate(self, image, text: str, max_new_tokens: int = 256):
+        message = []
+        if isinstance(image, list):
+            image__base64_list = [encode_image_to_base64(img) for img in image]
+            message_base = {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": text,
+                    },
+                ],
+            }
+            for image_base64 in image__base64_list:
+                message_base["content"].append(
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{image_base64}",
+                            "detail": "low",
+                        },
+                    }
+                )
+            message = [message_base]
+        else:
+
+            image_base64 = encode_image_to_base64(image)
+            # 画像で一言のお題
+            message = [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": text,
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{image_base64}",
+                                "detail": "low",
+                            },
+                        },
+                    ],
+                }
+            ]
+        response = self.client.chat.completions.create(
+            model=self.model_id,
+            messages=message,
+            max_tokens=max_new_tokens
+        )
+        return response.choices[0].message.content
+
+
+if __name__ == "__main__":
+    import requests
+    from PIL import Image
+
+    model = VLM()
+    image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(image_file, stream=True).raw)
+    print(model.generate(image, "What is in the image?"))
---
+++ examples/VILA_ja.py
@@ -0,0 +1,84 @@
+import torch
+from PIL import Image
+import requests
+import torch
+from llava.constants import IMAGE_TOKEN_INDEX
+from llava.conversation import conv_templates
+from llava.mm_utils import (
+    get_model_name_from_path,
+    process_images,
+    tokenizer_image_token,
+)
+from llava.model.builder import load_pretrained_model
+
+
+def load_images(image_files):
+    out = []
+    for image_file in image_files:
+        image = Image.open(image_file).convert("RGB")
+        out.append(image)
+    return out
+
+
+class VLM:
+    model_id = "llm-jp/VILA-ja"
+
+    def __init__(self) -> None:
+        model_checkpoint_path = "/model/sasagawa/VILA-ja/checkpoints/llm-jp-3-13b-instruct_siglip_mlp2xgelu_step-2_20241004/"
+        model_name = get_model_name_from_path(model_checkpoint_path)
+        print(model_name)
+        self.tokenizer, self.model, self.image_processor, self.context_len = (
+            load_pretrained_model(model_checkpoint_path, model_name)
+        )
+
+    def generate(self, image, text: str, max_new_tokens: int = 256):
+        qs = text
+        if "<image>" not in text:
+            if isinstance(image, list):
+                qs = "<image>\n" * len(image)  + text
+            else:
+                qs = "<image>\n" + text
+        print(qs)
+        print(image)
+        conv_mode = "llmjp_v3"
+        conv = conv_templates[conv_mode].copy()
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+        if isinstance(image, list):
+            images = image
+        else:
+            images = [image]
+        images_tensor = process_images(
+            images, self.image_processor, self.model.config
+        ).to(self.model.device, dtype=torch.float16)
+        input_ids = (
+            tokenizer_image_token(
+                prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt"
+            )
+            .unsqueeze(0)
+            .cuda()
+        )
+
+        with torch.inference_mode():
+            output_ids = self.model.generate(
+                input_ids,
+                images=[
+                    images_tensor,
+                ],
+                do_sample=False,
+                num_beams=1,
+                max_new_tokens=max_new_tokens,
+                use_cache=True,
+            )
+
+        outputs = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
+        return outputs
+
+
+if __name__ == "__main__":
+    model = VLM()
+    image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(image_file, stream=True).raw)
+    print(model.generate(image, "What is in the image?"))
+    print(model.generate([image, image], "What is in the image?"))
